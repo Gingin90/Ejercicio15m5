17b2a891dde70129a9e90d5f8855d4fb7250ec08
ginger cea <gingercea@gmail.com>
2023-07-18 19:39:59 -0400

    Proyecto 15m5 inicio del proyecto

---
+++ app/src/main/java/com/example/ejercicio15m5/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.ejercicio15m5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*pasos para craer recicler view
+*[]Adapter+ view holder
+* [] item layout
+*[] layoaut con recicler view
+*[] layout manager
+* [] obtener lista de datos
+* */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
